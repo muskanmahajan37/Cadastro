cc93a41090e0a8cc6c0694ef8ab2c60c52b00c6d
Athos F. Bernardo <80980718+AthosFB@users.noreply.github.com>
2021-08-15 16:24:30 -0300

    Continuação do A2F

--- Pacote-Dowload/tela_do_cadastro/verificação_de_conta.py
+++ Pacote-Dowload/tela_do_cadastro/verificação_de_conta.py
@@ -11,7 +11,26 @@ def verificacao(email, nome, senha):
     codigo = codigo.replace("[", "")
     codigo = codigo.replace("]", "")
     codigo = codigo.replace(",", "")
-
     mcursor.execute("use login;")
     mcursor.execute(f"insert into conf value (default, '{email}', '{nome}', '{senha}', '{codigo}');")
     enviar.enviarconfirm(email, codigo)
+
+
+def validador(email, codigo):
+    import mysql.connector
+    cnx = mysql.connector.connect(user="root", password="", host="127.0.0.1")
+    mcursor = cnx.cursor()
+    mcursor.execute("use login;")
+    mcursor.execute(f"select * from conf where email = '{email}';")
+    rows = mcursor.fetchall()
+    if len(rows) == 0:
+        print("Erro!!!")
+    else:
+        if rows[0][4] == codigo:
+            mcursor.execute("use login;")
+            mcursor.execute(f"insert into logi value (default, '{rows[0][1]}', '{rows[0][2]}', '{rows[0][3]}');")
+            mcursor.execute(f"delete from conf where email = '{rows[0][1]}'")
+            print("CONTA ADICIONADA!")
+        else:
+            print("Código inválido!")
+
